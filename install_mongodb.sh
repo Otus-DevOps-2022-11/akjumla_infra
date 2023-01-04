46a813a464046d2b1b103e72262a7b907b846e41
akjumla <120498178+akjumla@users.noreply.github.com>
2023-01-04 20:50:44 +0300

    mongo

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -9,13 +9,8 @@ echo "Check mongod status"
 sudo systemctl status mongod
 echo "Start mongod"
 sudo systemctl start mongod
-mongoStatus = $(sudo systemctl status mongod)
-expectedMongoStatus = "Failed to connect to bus: No such file or directory"
 echo "Check mongod status again"
 sudo systemctl status mongod
-if [$mongoStatus = $expectedMongoStatus]
-then
 sudo systemctl start mongodb.service
-fi
 echo "Add mongod to autostart"
 sudo systemctl enable mongod
